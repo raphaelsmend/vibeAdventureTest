f40c50e154c104da984ddd7eea8d52dd63c2ecb2
raphaelmendonca <raphael_mendonca@hotmail.com>
2024-02-22 03:40:45 -0300

    index adjsuts

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
                 {
                     path: '/',
                     name: 'dashboard',
-                    component: () => import('@/views/Dashboard.vue')
+                    component: () => import('@/views/vibe/test.vue')
                 },
                 {
                     path: '/uikit/formlayout',
